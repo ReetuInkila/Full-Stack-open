0140e460b2de3dde73b690ddc9ea0c3e37d0a48c
lrtinkiv <lrtinkiv@jyu.fi>
2024-03-09 10:07:13 +0200

    3.16

--- osa3/index.js
+++ osa3/index.js
@@ -1,14 +1,11 @@
-require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const Person = require('./models/person')
 const app = express()
+require('dotenv').config()
 app.use(express.static('dist'))
-app.use(express.json())
-app.use(cors())
 
+const Person = require('./models/person')
 
+const morgan = require('morgan')
 morgan.token('body', req => {
     return JSON.stringify(req.body)
 })
@@ -23,6 +20,22 @@ app.use(morgan(function (tokens, req, res) {
     ].join(' ')
 }))
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+}
+
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+
+
 app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 })
@@ -33,10 +46,15 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person)
+        if(person){
+            response.json(person)
+        }else{
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -58,12 +76,15 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
     })
-  })
+    .catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
