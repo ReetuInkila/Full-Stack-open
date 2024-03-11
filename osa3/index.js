bdd382bc44a7397ed9685a8a7ea5de4bccbba51b
lrtinkiv <lrtinkiv@jyu.fi>
2024-03-11 10:59:39 +0200

    3.20

--- osa3/index.js
+++ osa3/index.js
@@ -64,12 +64,6 @@ app.get('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
   
-    if (!body.number) {
-      return response.status(400).json({ 
-        error: 'Number missing' 
-      })
-    }
-  
     const person = new Person({
       name: body.name,
       number: body.number
@@ -91,12 +85,6 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
 app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
-
-    if (!body.number) {
-        return response.status(400).json({ 
-          error: 'Number missing' 
-        })
-    }
   
     const person = {
         name: body.name,
--- osa3/models/person.js
+++ osa3/models/person.js
@@ -13,12 +13,24 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: {    
-    type: String,    
-    minlength: 3,    
-    required: true
+    name: {    
+        type: String,    
+        minlength: 3,    
+        required: true
     },
-  number: String,
+    number:  {    
+        type: String,    
+        validate: {
+            validator: function(v) {
+                if(v.length<8){
+                    return false
+                }
+                return /^\d{2,3}-\d{4,}$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },    
+        required: true
+    }
 })
 
 personSchema.set('toJSON', {
