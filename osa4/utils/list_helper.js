0aae973fa50901deb6522154894a7af0fe9b8557
lrtinkiv <lrtinkiv@jyu.fi>
2024-03-12 14:04:24 +0200

    4.6

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -19,7 +19,39 @@ const favoriteBlog = (blogs) => {
     });
     return favorite
 }
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return null; // Palautetaan null, jos taulukko on tyhjä
+    }
+
+    // Luodaan objekti, jossa avaimena on kirjoittajan nimi ja arvona blogien määrä
+    const blogCounts = {};
+    blogs.forEach(blog => {
+        if (blog.author in blogCounts) {
+            blogCounts[blog.author]++;
+        } else {
+            blogCounts[blog.author] = 1;
+        }
+    });
+
+    // Etsitään kirjoittaja, jolla on eniten blogeja
+    let maxBlogsAuthor = '';
+    let maxBlogsCount = 0;
+    for (const author in blogCounts) {
+        if (blogCounts[author] > maxBlogsCount) {
+            maxBlogsCount = blogCounts[author];
+            maxBlogsAuthor = author;
+        }
+    }
+
+    return {
+        author: maxBlogsAuthor,
+        blogs: maxBlogsCount
+    };
+};
+
   
 module.exports = {
-    dummy, totalLikes, favoriteBlog
+    dummy, totalLikes, favoriteBlog, mostBlogs
 }
