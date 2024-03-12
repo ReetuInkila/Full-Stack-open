e24ddd80ee1668ee499bbae5b43eaebca2518755
lrtinkiv <lrtinkiv@jyu.fi>
2024-03-12 14:17:36 +0200

    4.7

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -16,42 +16,72 @@ const favoriteBlog = (blogs) => {
         if (favorite === null || element.likes >= favorite.likes) {
             favorite = element
         }
-    });
+    })
     return favorite
 }
 
 const mostBlogs = (blogs) => {
     if (blogs.length === 0) {
-        return null; // Palautetaan null, jos taulukko on tyhjä
+        return null // Palautetaan null, jos taulukko on tyhjä
     }
 
     // Luodaan objekti, jossa avaimena on kirjoittajan nimi ja arvona blogien määrä
-    const blogCounts = {};
+    const blogCounts = {}
     blogs.forEach(blog => {
         if (blog.author in blogCounts) {
-            blogCounts[blog.author]++;
+            blogCounts[blog.author]++
         } else {
-            blogCounts[blog.author] = 1;
+            blogCounts[blog.author] = 1
         }
-    });
+    })
 
     // Etsitään kirjoittaja, jolla on eniten blogeja
-    let maxBlogsAuthor = '';
-    let maxBlogsCount = 0;
+    let maxBlogsAuthor = ''
+    let maxBlogsCount = 0
     for (const author in blogCounts) {
         if (blogCounts[author] > maxBlogsCount) {
-            maxBlogsCount = blogCounts[author];
-            maxBlogsAuthor = author;
+            maxBlogsCount = blogCounts[author]
+            maxBlogsAuthor = author
         }
     }
 
     return {
         author: maxBlogsAuthor,
         blogs: maxBlogsCount
-    };
-};
+    }
+}
 
-  
+const mostLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return null // Palautetaan null, jos taulukko on tyhjä
+    }
+
+    // Luodaan objekti, jossa avaimena on kirjoittajan nimi ja arvona tykkäysten yhteismäärä
+    const likeCounts = {}
+    blogs.forEach(blog => {
+        if (blog.author in likeCounts) {
+            likeCounts[blog.author] += blog.likes
+        } else {
+            likeCounts[blog.author] = blog.likes
+        }
+    })
+
+    // Etsitään kirjoittaja, jonka blogeilla on eniten tykkäyksiä
+    let maxLikesAuthor = ''
+    let maxLikesCount = 0
+    for (const author in likeCounts) {
+        if (likeCounts[author] > maxLikesCount) {
+            maxLikesCount = likeCounts[author]
+            maxLikesAuthor = author
+        }
+    }
+
+    return {
+        author: maxLikesAuthor,
+        likes: maxLikesCount
+    }
+}
+
 module.exports = {
-    dummy, totalLikes, favoriteBlog, mostBlogs
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
