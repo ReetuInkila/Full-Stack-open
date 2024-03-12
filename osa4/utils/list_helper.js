5ad9421d828496715295f985cc46f51c6c5c3430
lrtinkiv <lrtinkiv@jyu.fi>
2024-03-12 12:02:18 +0200

    4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
     return 1
 }
+
+const totalLikes = (blogs)=> {
+    let total = 0
+    blogs.forEach(element => {
+        total += element.likes
+    })
+    return total 
+}
   
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
